772124df80b8f3e6a60aaaa801654a8d0e3b209a
JoharryJC <energycuenta@yahoo.com>
2022-05-24 21:07:09 +0200

    Selectores Anidados

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PaisesService } from '../../services/paises.service';
+import { PaisSmall } from '../../interfaces/paises.interface';
 
 @Component({
   selector: 'app-selector-page',
@@ -8,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectorPageComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    region: ['', Validators.required], 
+    pais: ['', Validators.required] 
+  })
+
+  //llenar selectores
+  regiones: string[] = [];
+  paises: PaisSmall[] = []; 
+
+
+  constructor(private fb: FormBuilder, 
+              private paisesService: PaisesService) { }
 
   ngOnInit(): void {
+    this.regiones = this.paisesService.regiones;
+
+    //Cuando cambie la region
+
+    this.miFormulario.get('region')?.valueChanges
+    .subscribe(region => {
+      console.log(region) 
+
+      this.paisesService.getPaisesPorRegion(region)
+      .subscribe(paises => {
+          console.log(paises);
+          this.paises = paises; 
+      })
+    }) 
+
+  }
+
+  guardar() {
+    console.log(this.miFormulario.value); 
   }
 
 }
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PaisSmall } from '../interfaces/paises.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesService {
 
-  constructor() { }
+  private baseUrl: string = 'https://restcountries.com/v2'; 
+
+  //private _regiones: string [] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']; 
+  private _regiones: string [] = ['EU', 'EFTA', 'CARICOM', 'PA', 'AU', 'USAN', 'EEU', 'AL', 'ASEAN', 'CAIS', 'CEFTA', 'NAFTA', 'SAARC']; 
+
+  get regiones (): string[] {
+    return [...this._regiones]; 
+  } 
+
+  constructor(private http: HttpClient) { }
+
+  getPaisesPorRegion (region: string): Observable<PaisSmall[]> {
+      const url: string = `${ this.baseUrl }/regionalbloc/${ region }?fields=alpha3Code,name`; 
+      console.log("url leida: ", url); 
+      return this.http.get<PaisSmall[]>(url); 
+  } 
+
 }
